097773913ad025f04cfee9ea0447af95415bbf20
Ivan Shaporov <ivan_shaporov@yahoo.com>
2021-05-14 18:38:58 -0700

    Minor updates.

--- Software/BlobUploader/BlobUpload.py
+++ Software/BlobUploader/BlobUpload.py
@@ -3,8 +3,12 @@ from os import environ
 from datetime import datetime
 import sys
 
-blob_name = datetime.now().strftime("%Y/%m/%d/%H_%M.h264")
+blob_name = datetime.now().strftime("%Y/%m/%d/%Y%m%d_%H%M.mp4")
+
+print(f'blob_name={blob_name}')
 
 blob = BlobClient(account_url=environ['BlobUploadUrl'], container_name='catfeedervideo', blob_name=blob_name, credential=environ['BlobUploadSas'])
 
 blob.upload_blob(sys.stdin.buffer.read())
+
+print(f'{blob_name} finished')
