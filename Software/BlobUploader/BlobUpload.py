f96b2b90b27a2a541c47bd10d63ee480a3747756
Ivan Shaporov <ivan_shaporov@yahoo.com>
2021-06-01 18:09:03 -0700

    Blob uploader: retries and logging.

--- Software/BlobUploader/BlobUpload.py
+++ Software/BlobUploader/BlobUpload.py
@@ -1,22 +1,35 @@
-from azure.storage.blob import BlobClient
-from os import environ, path
-from datetime import datetime
+import logging
 import sys
+from datetime import datetime
+from os import environ, path
 
+from azure.storage.blob import BlobClient
+
+
+logging.basicConfig()
+logger = logging.getLogger('BlobUpload')
+logger.setLevel(logging.INFO)
 
 def upload(filepath, blob_name):
     blob = BlobClient(account_url=environ['BlobUploadUrl'], container_name='catfeedervideo', blob_name=blob_name, credential=environ['BlobUploadSas'])
-    with open(filepath, "rb") as data:
-        blob.upload_blob(data)
+
+    for attempt in range(3):
+        try:
+            logger.info(f'{datetime.now()} {filepath} to {blob_name} attempt #{attempt}')
+            with open(filepath, "rb") as data:
+                blob.upload_blob(data)
+                break
+        except Exception as e:
+            logger.exception("Upload failed")
 
 filepath = path.splitext(sys.argv[1])[0] # without extension
 
 blob_name = datetime.now().strftime('%Y/%m/%d/%Y%m%d_%H%M')
 
-print(f'{datetime.now()} blob_name={blob_name}')
+logger.info(f'{datetime.now()} blob_name={blob_name}')
 
 #blob.upload_blob(sys.stdin.buffer.read())
 upload(filepath + '.jpg', blob_name + '.jpg')
 upload(filepath + '.mp4', blob_name + '.mp4')
  
-print(f'{datetime.now()} {blob_name} finished')
+logger.info(f'{datetime.now()} {blob_name} finished')
