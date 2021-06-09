bb439b1c7fa834de93113f3faab3e821839fdcf4
Ivan Shaporov <ivan_shaporov@yahoo.com>
2021-06-09 11:40:38 -0700

    Azure events.

--- Software/Services/FoodCycle.py
+++ Software/Services/FoodCycle.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import json
 import logging
 import time
-from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureLogHandler, AzureEventHandler
 
 import Light
 from AnalyzeImage import AnalyzeImage, GetImageUrl
@@ -12,6 +12,7 @@ from Camera import StartRecording, StartEncoding, StartExtracting, GetVideoName
 from FoodDispenser import Feed
 
 logger = logging.getLogger('CatFeeder')
+events = logging.getLogger('CatFeeder_events')
 
 def FullCycle(config, skipFood):
 
@@ -22,6 +23,8 @@ def FullCycle(config, skipFood):
 
         if not skipFood:
             Feed()
+        else:
+            logger.info('Skipping food')
         
         error = p.wait()
         if error != 0:
@@ -64,10 +67,17 @@ def FullCycle(config, skipFood):
             imageUrl = GetImageUrl(blobname, config)
             properties = {'custom_dimensions': {'imageUrl': imageUrl, 'tags': json.dumps(tags)}}
 
-            if (not foodPile) and (liftedTrunk or trunk or emptyPlate):
+            noFood = (not foodPile) and (liftedTrunk or trunk or emptyPlate)
+
+            if noFood:
                 logger.warning(f'Food cycle completed. No food.', extra=properties)
             else:
                 logger.info(f'Food cycle completed.', extra=properties)
+
+            eventValue = 'skipped' if skipFood else 'failed' if noFood else 'delivered'
+            properties['custom_dimensions']['eventType'] = 'Food'
+            properties['custom_dimensions']['eventValue'] = eventValue
+            events.info(f'Food cycle completed. Food {eventValue}.', extra=properties)
     except:
         logger.exception()
         Light.Off()
@@ -76,16 +86,17 @@ if __name__ == '__main__':
     now = datetime.now()
     import Config
 
-    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s')
     logger.addHandler(AzureLogHandler(connection_string=Config.AppInsightsConnectionString))
+    events.addHandler(AzureEventHandler(connection_string=Config.AppInsightsConnectionString))
+
+    logger.setLevel(logging.INFO)
+    events.setLevel(logging.INFO)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-noFood', '-n', action='store_true')
     parser.add_argument('-videoDuration', '-v', type=int, default=Config.VideoDuration)
     args = parser.parse_args()
-
-    if args.noFood: 
-        logger.info('Skipping food.')
 
     if args.videoDuration != Config.VideoDuration:
         Config.VideoDuration = args.videoDuration
